a51bdf9ad5f077d106bbf8ee90d280c82182ada6
wgz <ultranumblol@outlook.com>
2016-08-03 18:31:37 +0800

    add selectFileUploadActivity

--- app/src/main/java/wgz/com/cx_ga_project/activity/SelectFileUploadActivity.java
+++ app/src/main/java/wgz/com/cx_ga_project/activity/SelectFileUploadActivity.java
@@ -12,22 +12,6 @@ import wgz.com.cx_ga_project.base.BaseActivity;
 
 public class SelectFileUploadActivity extends BaseActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_select_file_upload);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
 
     @Override
     public int getLayoutId() {
