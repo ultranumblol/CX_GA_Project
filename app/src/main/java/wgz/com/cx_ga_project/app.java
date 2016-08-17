7710be3f3127bc7c69184a35a10cd01d8cb53551
wgz2 <ultranumblol@outlook.com>
2016-08-17 17:32:49 +0800

    上传图片功能完善,添加进度

--- app/src/main/java/wgz/com/cx_ga_project/app.java
+++ app/src/main/java/wgz/com/cx_ga_project/app.java
@@ -2,9 +2,12 @@ package wgz.com.cx_ga_project;
 
 import android.app.Application;
 import android.content.Intent;
+import android.os.Looper;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
@@ -15,6 +18,8 @@ import wgz.com.cx_ga_project.activity.StartNewFightActivity;
 import wgz.com.cx_ga_project.base.APIservice;
 import wgz.com.cx_ga_project.exception.LocalFileHandler;
 import wgz.com.cx_ga_project.service.GetGPSService;
+import wgz.com.cx_ga_project.util.ProgressListener;
+import wgz.com.cx_ga_project.util.ProgressResponseBody;
 import wgz.datatom.com.utillibrary.util.LogUtil;
 import wgz.datatom.com.utillibrary.util.ToastUtil;
 
@@ -42,6 +47,7 @@ public class app extends Application {
                 .addConverterFactory(ScalarsConverterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .client(defaultOkHttpClient())
                 .build();
 
 
@@ -53,6 +59,22 @@ public class app extends Application {
                 .connectTimeout(3, TimeUnit.SECONDS)
                 .writeTimeout(3, TimeUnit.SECONDS)
                 .readTimeout(3, TimeUnit.SECONDS)
+                .addNetworkInterceptor(new Interceptor() {
+                    @Override
+                    public okhttp3.Response intercept(Chain chain) throws IOException {
+                        okhttp3.Response orginalResponse = chain.proceed(chain.request());
+
+                        return orginalResponse.newBuilder()
+                                .body(new ProgressResponseBody(orginalResponse.body(), new ProgressListener() {
+                                    @Override
+                                    public void onProgress(long progress, long total, boolean done) {
+                                        LogUtil.e(Looper.myLooper()+"");
+                                        LogUtil.e("onProgress: " + "total ---->" + total + "done ---->" + progress);
+                                    }
+                                }))
+                                .build();
+                    }
+                })
                 .build();
         return client;
     }
